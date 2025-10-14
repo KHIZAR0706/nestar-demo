870ba16d08d741bcb2f8a1b2d4d3169f7273f15c
KHIZAR0706 <giyoskhujanazarov@gmail.com>
2025-10-14 12:22:55 +0900

    feat: organize and test pages router system

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,14 @@
 import Head from "next/head";
 import Image from "next/image";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Inter, Roboto_Mono } from "next/font/google";
 import styles from "@/styles/Home.module.css";
 
-const geistSans = Geist({
+const geistSans = Inter({
   variable: "--font-geist-sans",
   subsets: ["latin"],
 });
 
-const geistMono = Geist_Mono({
+const geistMono = Roboto_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
 });
@@ -22,18 +22,11 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div
-        className={`${styles.page} ${geistSans.variable} ${geistMono.variable}`}
-      >
+
+      <div className={`${styles.page} ${geistSans.variable} ${geistMono.variable}`}>
         <main className={styles.main}>
-          <Image
-            className={styles.logo}
-            src="/next.svg"
-            alt="Next.js logo"
-            width={180}
-            height={38}
-            priority
-          />
+          <h1>PAGES ROUTER</h1>
+
           <ol>
             <li>
               Get started by editing <code>pages/index.tsx</code>.
@@ -67,6 +60,7 @@ export default function Home() {
             </a>
           </div>
         </main>
+
         <footer className={styles.footer}>
           <a
             href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
@@ -115,3 +109,4 @@ export default function Home() {
     </>
   );
 }
+
