71a5c8e120d0a7b43d8f4c5ee889556c00f33c01
KHIZAR0706 <giyoskhujanazarov@gmail.com>
2025-10-21 15:59:05 +0900

    feat: develop apollo client connection

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,13 +7,35 @@ import TopAgents from "@/libs/components/homepage/TopAgents";
 import useDeviceDetect from "@/libs/hooks/useDeviceDetect";
 import { Stack } from "@mui/material";
 import { NextPage } from "next";
+import { useQuery } from "@apollo/client";
+import { GET_PROPERTIES } from "@/apollo/user/query";
 import "swiper/css";
 import "swiper/css/pagination";
 import "swiper/css/navigation";
 
 
+
 const Home: NextPage = () => {
   const device = useDeviceDetect();
+
+  const {
+    loading: getPropertiesLoading,
+    data: getPropertiesData,
+    error: getPropertiesError,
+    refetch: getPropertiesRefetch,
+  } = useQuery(GET_PROPERTIES, {
+    fetchPolicy: "network-only",
+    variables: {
+      input: {
+        page: 1,
+        limit: 5,
+        sort: "createdAt",
+        direction: "DESC",
+        search: {},
+      },
+    },
+  });
+  console.log("getPropertiesData => ", getPropertiesData);
 
   if (device === "mobile") {
     return <Stack>HOMEPAGE MOBILE</Stack>;
